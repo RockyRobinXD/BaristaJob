9e9f46a8852460ed21bac6a8185f30aaf55ae8c6
Robin Van der Borght <Robin.VanderBorght@student.hogent.be>
2026-08-17 18:49:31 +0200

    Startup project

---
+++ src/main/java/rockyrobin/be/baristajob/BaristaJobApplication.java
@@ -0,0 +1,13 @@
+package rockyrobin.be.baristajob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaristaJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaristaJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rockyrobin/be/baristajob/BaristaJobApplicationTests.java
@@ -0,0 +1,13 @@
+package rockyrobin.be.baristajob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaristaJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
