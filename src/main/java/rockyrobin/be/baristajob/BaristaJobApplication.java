2c5c95392dd7ffe0798acc664157fc767c2a54ba
Robin Van der Borght <Robin.VanderBorght@student.hogent.be>
2026-08-23 17:18:49 +0200

    Added some config

--- src/main/java/rockyrobin/be/baristajob/BaristaJobApplication.java
+++ src/main/java/rockyrobin/be/baristajob/BaristaJobApplication.java
@@ -2,12 +2,19 @@ package rockyrobin.be.baristajob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BaristaJobApplication {
+public class BaristaJobApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(BaristaJobApplication.class, args);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/home");
     }
 
 }
